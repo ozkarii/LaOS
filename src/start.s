1ab9944f82178a0d72acfd47db5b1f0c7dfbf9cf
Oskari Heinonen <oskariheinonen22@gmail.com>
2025-08-10 18:11:12 +0300

    startup: add bss zeroing and space for heap

--- src/start.s
+++ src/start.s
@@ -1,11 +1,23 @@
 .global _Reset
 _Reset:
-    mrs x1, mpidr_el1
-    and x1, x1, #0xFF
-    cbnz x1, _Infinite_loop
+    mrs x0, mpidr_el1
+    and x0, x0, #0xFF
+    cbnz x0, _Infinite_loop
 
-    ldr x30, =stack_top	     // setup stack
-    sub x30, x30, #0x1000    // Start 4KB below stack_top for safety
+    ldr x1, =bss_start
+    ldr x2, =bss_end
+    cmp x1, x2
+    beq _Set_sp
+
+    mov x3, #0  // offset
+_Zero_bss_loop:
+    strb wzr, [x1], #1    // Store zero and post-increment x1
+    cmp x1, x2            // Compare current address with bss_end
+    blt _Zero_bss_loop    // Continue if current address < bss_end
+
+
+_Set_sp:
+    ldr x30, =stack_top  // setup stack
     mov sp, x30
     
     bl c_entry
