d7f72cc4f542015901c7f7ec4879136537fdddc7
Laudes Centurion <laudes.centurionrivas@tuni.fi>
2025-08-10 17:17:59 +0300

    snprintf: modify to handle safely NULL parameters

--- src/libc/snprintf.c
+++ src/libc/snprintf.c
@@ -1,6 +1,9 @@
 #include "include/stdio.h"
 
 int snprintf(char* buf, size_t size, const char* format, ...) {
+    if (buf == NULL || format == NULL) {
+        return -1;
+    }
     va_list args;
     va_start(args, format);
     int result = vsnprintf(buf, size, format, args);
