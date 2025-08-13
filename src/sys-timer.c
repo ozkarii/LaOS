10c6838f5566f3cb025b3b116df004cd84ca66aa
Oskari Heinonen <oskariheinonen22@gmail.com>
2025-08-13 21:33:21 +0300

    sys-timer: add basic timer functionality

--- src/sys-timer.c
+++ src/sys-timer.c
@@ -1,16 +1,40 @@
 #include "sys-timer.h"
 #include "platform.h"
 
-static inline void sys_timer_write_reg(uint64_t offset, uint32_t val) {
-    *(volatile uint32_t*)(SYS_TIMER_BASE + offset) = val;
+static inline void sys_timer_write_reg32(uint64_t offset, uint32_t val) {
+  *(volatile uint32_t*)(SYS_TIMER_BASE + offset) = val;
 }
 
-static inline uint32_t sys_timer_read_reg(uint64_t offset) {
-    return *(volatile uint32_t*)(SYS_TIMER_BASE + offset);
+static inline uint32_t sys_timer_read_reg32(uint64_t offset) {
+  return *(volatile uint32_t*)(SYS_TIMER_BASE + offset);
 }
 
 uint64_t sys_timer_get_value(void) {
-    // Using 32-bit accesses to be safe for now
-    return (((uint64_t)sys_timer_read_reg(CHI) << 32u)
-           | (uint64_t)sys_timer_read_reg(CLO));
+  // Using 32-bit accesses to be safe for now
+  return (((uint64_t)sys_timer_read_reg32(CHI) << 32u)
+         | (uint64_t)sys_timer_read_reg32(CLO));
+}
+
+int sys_timer_set_comp(uint32_t comp_reg, uint32_t value) {
+  if (comp_reg > 3) {
+    return -1;
+  }
+  sys_timer_write_reg32(C0 + (comp_reg * 4), value);
+  return 0;
+}
+
+int sys_timer_check_match(uint32_t comp_reg) {  
+  if (comp_reg > 3) {
+    return -1;
+  }
+  uint32_t cs = sys_timer_read_reg32(CS);
+  return (cs & (CS_M0 << comp_reg)) ? 1 : 0;
+}
+
+int sys_timer_clear_match(uint32_t comp_reg) {
+  if (comp_reg > 3) {
+    return -1;
+  }
+  sys_timer_write_reg32(CS, sys_timer_read_reg32(CS) & (CS_M0 << comp_reg));
+  return 0;
 }
--- src/sys-timer.h
+++ src/sys-timer.h
@@ -3,9 +3,23 @@
 
 #include <stdint.h>
 
-#define CLO 0x4
-#define CHI 0x8
+#define CS     0x00
+#define CS_M0  1u
+#define CS_M1  (1u << 1u)
+#define CS_M2  (1u << 2u)
+#define CS_M3  (1u << 3u)
+
+#define CLO    0x04
+#define CHI    0x08
+#define C0     0x0c
+#define C1     0x10
+#define C2     0x14
+#define C3     0x18
+
 
 uint64_t sys_timer_get_value(void);
+int sys_timer_set_comp(uint32_t comp_reg, uint32_t value);
+int sys_timer_check_match(uint32_t comp_reg);
+int sys_timer_clear_match(uint32_t comp_reg);
 
 #endif // SYS_TIMER_H
