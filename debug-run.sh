441e969b1c72a2d06909191ff09b4646d7eb6c60
Oskari Heinonen <oskariheinonen22@gmail.com>
2026-01-04 16:32:49 +0200

    fix: path to kernel.elf in scripts

--- debug-run.sh
+++ debug-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gdb --ex "run" --args qemu-system-aarch64 -machine raspi4b -kernel build/kernel.elf -smp 4 -nographic -s -S $@
+gdb --ex "run" --args qemu-system-aarch64 -machine raspi4b -kernel build/kernel/kernel.elf -smp 4 -nographic -s -S $@
