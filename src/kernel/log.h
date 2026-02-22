c58b0d1edb3d2ca54377c6ceb62ca764e04f759e
Oskari Heinonen <oskariheinonen22@gmail.com>
2026-02-22 13:48:50 +0200

    feat: new log categories

--- src/kernel/log.h
+++ src/kernel/log.h
@@ -4,5 +4,9 @@
 #define LOG_KERNEL  "[general] "
 #define LOG_SCHED   "[sched]   "
 #define LOG_SEM     "[k_sem]   "
+#define LOG_MMU     "[mmu]     "
+#define LOG_VFS     "[vfs]     "
+#define LOG_RAMFS   "[ramfs]   "
+#define LOG_FS      "[fs]      "
 
 #endif // LOG_H
