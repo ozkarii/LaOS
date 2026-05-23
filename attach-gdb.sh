ca7ff02c145f25375357e112f64edfcaa54ca3ac
Oskari Heinonen <oskariheinonen22@gmail.com>
2026-05-23 21:43:33 +0900

    chore: fix kernel symbol path

--- attach-gdb.sh
+++ attach-gdb.sh
@@ -10,4 +10,4 @@ if [ -z $GDB_CMD ]; then
 	fi
 fi
 
-$GDB_CMD --ex "set arch aarch64" --ex "symbol build/kernel/kernel.elf" --ex "target remote localhost:1234" --ex "tui layout split" --ex "tui focus cmd" $@
+$GDB_CMD --ex "set arch aarch64" --ex "symbol build/src/kernel/kernel" --ex "target remote localhost:1234" --ex "tui layout split" --ex "tui focus cmd" $@
