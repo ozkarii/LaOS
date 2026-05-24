618e9576884e3b15597075e423fbffb86d0ea7be
Oskari Heinonen <oskariheinonen22@gmail.com>
2026-05-24 14:23:25 +0900

    refactor: export syscall-common.h

--- src/user/common/syscall-user.c
+++ src/user/common/syscall-user.c
@@ -1,21 +1,7 @@
 #include "sys/types.h"
 #include "unistd.h"
 
-// Process
-#define SYS_EXIT    0
-#define SYS_FORK    1
-#define SYS_EXEC    2
-#define SYS_GETPID  3
-
-// Scheduling
-#define SYS_SLEEP   10
-
-// Files
-#define SYS_OPEN    20
-#define SYS_WRITE   21
-#define SYS_READ    22
-#define SYS_CLOSE   23
-
+#include "syscall-common.h"
 
 
 /*
